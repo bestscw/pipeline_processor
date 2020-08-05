509a4b00bcc1f007ce76a13e88a4a8201b7a152d
sunchangwei <sunchangwei@agotoz.com>
2020-08-05 16:47:10 +0800

    first commit

--- demo/demo.php
+++ demo/demo.php
@@ -1,34 +1,38 @@
 <?php
-
-include __DIR__ . DIRECTORY_SEPARATOR . 'src/Loader.php'; // 引入加载器
-spl_autoload_register('Loader::autoload'); // 注册自动加载
+include 'vendor/autoload.php';
 
 use \PipelineProcessor\PipelineProcessor;
 use \PipelineProcessor\Handler\OrderHandler;
 use \PipelineProcessor\Handler\PayHandler;
 use \PipelineProcessor\Registry;
 
-// 创建一些处理器
+//创建一些处理器
 $order = new \PipelineProcessor\Handler\OrderHandler();
 $pay = new PayHandler();
 
+//创建一些通道
 $application = new \PipelineProcessor\PipelineProcessor('application');
 $api = new \PipelineProcessor\PipelineProcessor('api');
 
 // 或者克隆第一个，只是改变下通道
 $security = $api->withName('security');
 
+//对通道应用哪些处理器
 $application->pushHandler($pay);
+$application->pushHandler($order);
+
 $api->pushHandler($pay);
 $security->pushHandler($pay);
+
 
 $application1 = $application->withName('application1');
 $api1 = $application->withName('api1');
 
+//通道的另一种调用方式
 Registry::addLogger($application1);
 Registry::addLogger($api1);
 
-
+//通道开始处理
 $application->info('application');
 $api->info('api');
 $security->info('security');
